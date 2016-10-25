1a81981ee7c2a72ba45b4e204825f280bc5d025a
gom <gomgom68@gmail.com>
2016-10-25 20:13:41 +0900

    Fix ansible-playbook

--- play.sh
+++ play.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-
 if ! type ansible > /dev/null 2>&1; then
     sudo apt-get install -y python-pip python-dev libffi-dev libssl-dev
     sudo pip install -U pip ansible
 fi
-ansible-playbook -i hosts localhost playbook.yml 
+ansible-playbook -i hosts -c local playbook.yml
